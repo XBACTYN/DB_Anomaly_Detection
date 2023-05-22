4bdac02a30b6f954f9925908d45d2588234eea00
Pavel <71969484+XBACTYN@users.noreply.github.com>
2023-05-22 07:15:33 +0400

    Add files via upload

--- RandomForest.py
+++ RandomForest.py
@@ -5,17 +5,27 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 import time
 import itertools
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, roc_auc_score, roc_curve, f1_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import svm
+
 
 system_random = random.SystemRandom()
 # source_Xy_train = 'data\\train\\GENERATED_transaction_vectors_train_1.json'
 # source_Xy_test = 'data\\test\\GENERATED_transaction_vectors_test_1.json'
-#source_Xy_train = 'data\\train\\GENERATED_classic_vectors_valid.json'   #110069
-#source_Xy_train = 'data\\test\\GENERATED_classic_vectors_test.json'      # 11017
-#source_Xy_train = 'data\\train\\GENERATED_classic_vectors_train.json'    #110069
-
+#source_Xy_train = 'data\\train\\GENERATED_classic_vectors_valid.json'
+source_Xy_test = 'data\\test\\GENERATED_classic_vectors_test.json'
+source_Xy_train = 'data\\train\\GENERATED_classic_vectors_train.json'
+source_Xy_test_1 = 'data\\test\\GENERATED_transaction_vectors_test.json'
+source_Xy_train_1 = 'data\\train\\GENERATED_transaction_vectors_train.json'
+result_path = 'results\\'
+classes = [1,2,3,4,5,6,7,8,9,10,11]
 def unpack_data(path):
     transactions_list = []
     role_list = []
@@ -28,46 +38,39 @@ def unpack_data(path):
         query_list.append(np.array(data[i].get('query')))
     X = np.array(query_list)
     y = np.array(role_list)
-    #print(transactions_list)
     print('unpacked')
-    return X,y,transactions_list
+    return X,y#,transactions_list
 
 def make_anomalies(y_test,percent):
-    list1 = [1,2,3,4,5,6,7,8,9,10,11]
+
     count = int(len(y_test)*percent/100)
     print(f'Будет заменено {count}  первых значений ролей из {len(y_test)}, процент измененных {percent}.')
     y_test_anomalies = y_test.copy()
     for i in range(0,count):
-        list2 = list1.copy()
+        list2 = classes.copy()
         list2.remove(y_test_anomalies[i])
         y_test_anomalies[i] = system_random.choice(list2)
     return y_test_anomalies,count
 
-def plot_confusion_matrix(cm, str,classes,normalize = False,
-                          title='Confusion matrix',
-                          cmap='OrRd'): # can change color
-    plt.figure(figsize = (10, 10))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title, size = 24)
-    plt.colorbar(aspect=4)
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45, size = 14)
-    plt.yticks(tick_marks, classes, size = 14)
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    # Label the plot
-    for i, j in itertools.product(range(cm.shape[0]),  range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-             fontsize = 20,
-             horizontalalignment="center",
-             color="white" if cm[i, j] > thresh else "black")
-        plt.grid(None)
-        plt.tight_layout()
-        plt.ylabel(f'{str} label', size = 18)
-        plt.xlabel('Predicted label', size = 18)
-    plt.savefig(f'{str}.jpg')
 
-def RandomForest(X_train,y_train,X_test,y_test_anomalies,y_test,count):
+def plot_confusion_matrix(y_true,y_pred,name,vector_type):
+    fig, ax = plt.subplots(figsize=(10,10))
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    ax = sns.heatmap(cf_matrix, annot=True, cmap='OrRd')
+
+    ax.set_title(' Матрица ошибок')
+    ax.set_xlabel('Предсказанные значения классов')
+    ax.set_ylabel(f'{name} значения классов')
+
+    ## Ticket labels - List must be in alphabetical order
+    ax.xaxis.set_ticklabels(classes)
+    ax.yaxis.set_ticklabels(classes)
+    #fig.savefig(f'{result_path}{vector_type}_confus_{name}_SVM.png')
+
+
+
+
+def RandomForest(X_train,y_train,X_test,y_test_anomalies,y_test,count,vector_type):
     print('start forest')
     model = RandomForestClassifier()
     start_time = time.time()
@@ -92,33 +95,54 @@ def RandomForest(X_train,y_train,X_test,y_test_anomalies,y_test,count):
 
     accuracy_score(y_test, y_pred)
     print(f"The accuracy of the model is {round(accuracy_score(y_test, y_pred), 3) * 100} %")
-    cm = confusion_matrix(y_test, y_pred)
-    cm2 = confusion_matrix(y_test_anomalies[:count],y_pred[:count])
-    plot_confusion_matrix(cm,'True', classes=[1, 2,3,4,5,6,7,8,9,10,11],title='Confusion Matrix')
-    plot_confusion_matrix(cm2, 'Anomalies',classes=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11], title='Confusion Matrix')
+    plot_confusion_matrix(y_test,y_pred,'Истинные',vector_type)
+    plot_confusion_matrix(y_test_anomalies[:count],y_pred[:count],'Аномальные',vector_type)
     plt.show()
-    # param_grid = {
-    #     'n_estimators': [25, 50, 100, 150],
-    #     'max_features': ['sqrt', 'log2', None],
-    #     'max_depth': [3, 6, 9],
-    #     'max_leaf_nodes': [3, 6, 9],
-    # }
-    #
-    # grid_search = GridSearchCV(RandomForestClassifier(),
-    #                            param_grid=param_grid)
-    # grid_search.fit(X_train, y_train)
-    # print(grid_search.best_estimator_)
+    #precision_recall_f1_metrics(y_test,y_pred)
 
-X_train,y_train,transactions = unpack_data(source_Xy_train)
-print(len(transactions))
-# print(len(X_train[0]))
-# print(len(X_train))
-# lst = list(zip(X_train,y_train))
-# random.shuffle(lst)
-# X_train,y_train = zip(*lst)
-# X_test, y_test = unpack_data(source_Xy_test)
-# y_test_anomalies,count = make_anomalies(y_test,15)
+def Gauss(X_train,y_train,X_test,y_test_anomalies,y_test,count,vector_type):
+    gnb = GaussianNB()
+    gnb.fit(X_train, y_train)
+    y_pred = gnb.predict(X_test)
+    accuracy_score(y_test, y_pred)
 
+    print(classification_report(y_pred, y_test))
+    anomalies_count = count
+    print(y_pred[:count])
+    print(y_test_anomalies[:count])
+    diff_values = sum(el1 != el2 for el1, el2 in zip(y_pred[:count], y_test_anomalies[:count]))
+    print(diff_values)
+    detection_rate = (diff_values / anomalies_count) * 100
+    print('anomalies count ', anomalies_count)
+    print('detection rate ', detection_rate)
+    plot_confusion_matrix(y_test, y_pred, 'Истинные',vector_type)
+    plot_confusion_matrix(y_test_anomalies[:count], y_pred[:count], 'Аномальные',vector_type)
+    plt.show()
 
 
-# RandomForest(X_train,y_train,X_test,y_test_anomalies,y_test,count)
+def SVM(X_train,y_train,X_test,y_test_anomalies,y_test,count,vector_type):
+    clf = svm.SVC(decision_function_shape='ovr')
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(classification_report(y_pred, y_test))
+    anomalies_count = count
+    print(y_pred[:count])
+    print(y_test_anomalies[:count])
+    diff_values = sum(el1 != el2 for el1, el2 in zip(y_pred[:count], y_test_anomalies[:count]))
+    print(diff_values)
+    detection_rate = (diff_values / anomalies_count) * 100
+    print('anomalies count ', anomalies_count)
+    print('detection rate ', detection_rate)
+    plot_confusion_matrix(y_test, y_pred, 'Истинные', vector_type)
+    plot_confusion_matrix(y_test_anomalies[:count], y_pred[:count], 'Аномальные', vector_type)
+    plt.show()
+
+X_train,y_train = unpack_data(source_Xy_train_1)
+X_test, y_test = unpack_data(source_Xy_test_1)
+y_test_anomalies,count = make_anomalies(y_test,25)
+
+
+#RandomForest(X_train,y_train,X_test,y_test_anomalies,y_test,count,'quiplet')
+#RandomForest(X_train,y_train,X_test,y_test_anomalies,y_test,count,'hexplet')
+#Gauss(X_train,y_train,X_test,y_test_anomalies,y_test,count,'hexplet')
+#SVM(X_train,y_train,X_test,y_test_anomalies,y_test,count,'quiplet')
